15e103debe7994e3956d4ca05068673bf5547f31
masterj <ronchaos@gmail.com>
2018-10-31 21:33:53 +0900

    * update mail code and real_trry??

---
+++ py3/Algorithms/dynamic_programming/real_tree.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def print_line(line):
+    print("****\n")
+
+
+def grid_line(block):
+
+
+def main():
+    grid_line(99)
+
+if __name__ == '__main__':
+    main()
