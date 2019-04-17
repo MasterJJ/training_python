2b7e32029c28706f9f3481fd68bdcec8a7f0b5da
masterj <ronchaos@gmail.com>
2019-04-17 18:03:08 +0900

    update python_filter

--- training_py_game/example.py
+++ training_py_game/example.py
@@ -1,3 +1,9 @@
 is_even = lambda x: x % 2 == 0
 print(is_even(3))
 print(is_even(2))
+
+# filter
+input_raw_data = [0, 1, 2, 3, 4, 5,]
+get_filter_data = list(filter(lambda a: a % 2 == 0, input_raw_data))
+
+print(get_filter_data)
